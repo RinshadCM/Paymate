3fd0d1e7f79d464c42fdad8534a83cefbc9fc05a
Mohammed Rinshad C M <rinshadcm34@gmail.com>
2022-12-02 14:20:24 +0530

    Login Page Created

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,72 @@
+import { ANALYZE_FOR_ENTRY_COMPONENTS, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  aim= "Your Perfect Banking Partner"
+  data="Enter Account Number"
+  acno=''
+  psw=''
+
+    userDetails:any={
+      1000:{acno:1000,username:"Anu",password:123,balance:0,},
+      1001:{acno:1001,username:"Amal",password:123,balance:0,},
+      1002:{acno:1002,username:"Arun",password:123,balance:0,},
+      1003:{acno:1003,username:"Megha",password:123,balance:0,}
+    }
+
+    // login(){
+    //   // alert('Login Clicked')
+    //   var acno=this.acno
+    //   var pass=this.pass
+    //   var userDetails=this.userDetails
+
+    //   if(acno in userDetails){
+    //     if(pass==userDetails[acno]["password"]){
+    //       alert('Login SUccess')
+    //     }
+    //     else{
+    //       alert('Incorrect Password')
+    //     }
+    //   }
+    //   else{
+    //     alert('Incorrect Username')
+    //   }
+    // }
+    login(a:any,b:any){
+      // alert('Login Clicked')
+      this.acno=a.value
+      this.psw=b.value
+
+      var acno=this.acno
+      var psw=this.psw
+      var userDetails=this.userDetails
+
+      if(acno in userDetails){
+        if(psw==userDetails[acno]["password"]){
+          alert('Login Success')
+        }
+        else{
+          alert('Incorrect Password')
+        }
+      }
+      else{
+        alert('Incorrect Username')
+      }
+    }
+
+
+    // acnoChange(event:any){
+    //   this.acno=event.target.value
+      
+    // }
+    // pswChange(event:any){
+    //   this.psw=event.target.value
+    //   console.log(this.psw);
+      
+    // }
+  }
